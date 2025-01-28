d3e1a58da74d99deb342e628a17b79c919744c4a
smarterToby <84527857+smarterToby@users.noreply.github.com>
2025-01-28 17:31:39 +0100

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,41 @@
-import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata, Viewport } from 'next';
+import { Analytics } from '@vercel/analytics/next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 const metaKeywords: string[] = [
-  "WhatsApp chat starter",
-  "Direct WhatsApp messages",
-  "WhatsApp without saving contacts",
-  "Quick messages WhatsApp",
-  "WhatsApp link generator",
-  "WhatsApp web tool",
-  "WhatsApp chat without saving",
-  "WhatsApp direct message",
-  "WhatsApp number chat",
-  "Simplify WhatsApp communication",
-  "Start WhatsApp chat with number"
+  'WhatsApp chat starter',
+  'Direct WhatsApp messages',
+  'WhatsApp without saving contacts',
+  'Quick messages WhatsApp',
+  'WhatsApp link generator',
+  'WhatsApp web tool',
+  'WhatsApp chat without saving',
+  'WhatsApp direct message',
+  'WhatsApp number chat',
+  'Simplify WhatsApp communication',
+  'Start WhatsApp chat with number',
 ];
 
 export const metadata: Metadata = {
-  title: "WhatsNow",
-  description: "Start a WhatsApp chat without saving contacts",
-  keywords: metaKeywords
+  title: 'WhatsNow',
+  description: 'Start a WhatsApp chat without saving contacts',
+  keywords: metaKeywords,
 };
 
 export const viewport: Viewport = {
-  themeColor: "#010203"
-}
+  themeColor: '#010203',
+};
 
 export default function RootLayout({
   children,
@@ -42,11 +43,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
